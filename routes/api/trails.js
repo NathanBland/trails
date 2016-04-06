28350d223d5b2d977094ebd6acbeeecf772dc519
Nathan Bland <nathan.bland@gmail.com>
2016-04-06 17:41:15 -0600

    ignore c9 files

--- routes/api/trails.js
+++ routes/api/trails.js
@@ -3,6 +3,8 @@
 var express = require('express')
 var router = module.exports = express.Router()
 var Trail = require('../../models/trail')
+var utils = require('./utils')
+
 router.route('/') // this is /api/trails
     .get(function(req, res, next) {
       // query string will need updating.
@@ -32,7 +34,6 @@ router.route('/') // this is /api/trails
         err => res.status(500).json({'error': 'Internal Server Error', err })
        )
     })
-    
 router.route('/:id') // this is /api/trails/:id
     .get(function(req, res, next) {
       Trail.findOne({_id: req.params.id})
@@ -42,3 +43,43 @@ router.route('/:id') // this is /api/trails/:id
           err => res.status(500).json({'error': 'Internal Server Error', err })
         )
     })
+    
+    
+// router.route('/:z/:x/:y')
+// .get(function(req, res){
+//     const bounds = utils.getTileBounds(req.params)
+//     const top = bounds.ne.lat
+//     const bottom = bounds.sw.lat
+//     const left = bounds.sw.lng
+//     const right = bounds.ne.lng
+//     var query = {}
+//       if (req.query.center) {
+//         var pointArray = JSON.parse(req.query.center)
+//         query = {
+//           geometry: {
+//             $geoIntersects: {
+//               $geometry: {
+//                 type: "Polygon", 
+//                 coordinates: [[
+//                   [left, bottom],
+//                   [left, top],
+//                   [right, top],
+//                   [right, bottom],
+//                   [left, bottom]
+//                 ]]
+//               }
+//             }
+//           }
+//         }
+//       }
+//     Trail.find(query)
+//       .limit(100)
+//       .sort('')
+//       .exec()
+//       .then(
+//         trails => {
+//           res.status(200).json(trails)
+//         },
+//         err => res.status(500).json({'error': 'Internal Server Error', err })
+//       )
+// })
