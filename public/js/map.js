20e0e43ad34124c0c300f0e2e985c348c2e821e0
Nathan Bland <nathan.bland@gmail.com>
2016-04-07 00:05:15 +0000

    updates to maps and things

--- public/js/map.js
+++ public/js/map.js
@@ -1,20 +1,27 @@
 /*global L*/
 /*global fetch*/
 
+var myStyle = {
+    "color": "#006400",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+
 var map = L.map('map', {
   center: [39.5501, -105.7821],
   zoom: 10
 });
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//'http://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}'
+//
+L.tileLayer('https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 var GeoJSON;
 getGeoJSON()
 map.addEventListener('moveend', getGeoJSON);
-
 map.addEventListener('resize', getGeoJSON);
-
 function getGeoJSON() {
   var center = map.getBounds().getCenter();
   fetch('/api/trails?center=' + JSON.stringify([center.lng, center.lat]))
@@ -26,9 +33,25 @@ function getGeoJSON() {
         map.removeLayer(GeoJSON); //remove the old GeoJSON
       }
       GeoJSON = L.geoJson(newJSON, {
+        style: myStyle,
         onEachFeature: function eachFeature(feature, layer){
-          layer.bindPopup(feature.properties.NAME)
+          layer.bindPopup(feature.properties.NAME || 'No name given')
         }
       }).addTo(map); //Add the new GeoJSON
     });
 }
+
+
+// var geojsonURL = '/api/trails/{z}/{x}/{y}';
+// var geojsonTileLayer = new L.TileLayer.GeoJSON(geojsonURL, {
+//         clipTiles: true,
+//         unique: function (feature) {
+//             return feature._id; 
+//         }
+//     }, {
+//       style: myStyle,
+//       onEachFeature: function eachFeature(feature, layer){
+//         layer.bindPopup(feature.properties.NAME || 'No name given')
+//       }
+//     }
+// ).addTo(map);
