2b595f746190045ae492e3d906432cf394d4ae27
Joel <jgimbel@live.com>
2016-04-10 03:40:27 -0500

    use regex to split

--- convert_geoJSON.js
+++ convert_geoJSON.js
@@ -1,27 +1,27 @@
 function fixCoordinates(coor){
-    if(Array.isArray(coor[0])) return coor.map(fixCoordinates)
-    return [coor[0], coor[1]]
+  if(Array.isArray(coor[0])) return coor.map(fixCoordinates)
+  return [coor[0], coor[1]]
 }
 
 const GeoJSON = require('./data/points_filtered_compact.json')
 
 const json = GeoJSON .map(feature => {
-    feature = {
-        type: feature.type,
-        properties: feature.properties,
-        geometry: {
-            type: feature.geometry.type,
-            coordinates: feature.geometry.coordinates
-        }
+  feature = {
+    type: feature.type,
+    properties: feature.properties,
+    geometry: {
+      type: feature.geometry.type,
+      coordinates: feature.geometry.coordinates
     }
-    if(feature.properties.other_tags){
-        const others = feature.properties.other_tags.split(',').reduce((obj, tag) => {
-            const a = tag.split('=>').map(key => key.replace('"', '').replace('"', ''))
-            obj[a[0]] = a[1] || ''
-            return obj
-        }, {})
-        feature.properties = Object.assign({}, feature.properties, others)
-    }
-    return feature
+  }
+  if(feature.properties.other_tags){
+    const others = feature.properties.other_tags.split(/("[^"]+"=>\"[^"]+",?)/g).reduce((obj, tag) => {
+      const a = tag.split('=>').map(key => key.replace('"', '').replace('"', ''))
+      obj[a[0]] = a[1] || ''
+      return obj
+    }, {})
+    feature.properties = Object.assign({}, feature.properties, others)
+  }
+  return feature
 })
 json.forEach(feature => console.log(JSON.stringify(feature)))
