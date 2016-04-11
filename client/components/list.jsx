4b3e5c5c8cf65ac0e14f9fb08ffe5ee2f3eced5e
Joel Gimbel <jgimbel@live.com>
2016-04-11 14:47:49 -0500

    fix list

--- client/components/list.jsx
+++ client/components/list.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { list } from '../actions/map'
+import { list as listActions } from '../actions/map'
 const list = ({
   trails,
   actions
@@ -45,6 +45,6 @@ export default connect(state => ({
 }),
 dispatch => ({
   actions: {
-    list: bindActionCreators(list, dispatch)
+    list: bindActionCreators(listActions, dispatch)
   }
 }))(list)
