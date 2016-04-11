dc8e617266dab62bf80728969e11c00b084b2b53
Joel Gimbel <jgimbel@live.com>
2016-04-11 14:48:07 -0500

    another list fix

--- client/components/list.jsx
+++ client/components/list.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { list as listActions } from '../actions/map'
+import { list as listActions } from '../actions'
 const list = ({
   trails,
   actions
