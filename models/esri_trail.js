8e0afbd1dd372f0e7341f8d3a8220878cbc3eefc
Nathan Bland <nathan.bland@gmail.com>
2016-04-12 12:28:27 -0600

    work on props and type of geometry for esri

--- models/esri_trail.js
+++ models/esri_trail.js
@@ -7,11 +7,30 @@ const EsriTrail = new Schema({
   geometry: {
     'type': {
       type: String,
-      default: 'MultiLineString'
+      default: 'Polygon'
     },
     coordinates: Schema.Types.Mixed
   },
-  properties: Schema.Types.Mixed
+  properties: {
+    ATV: Number,
+    BIKE: Number,
+    DATA_SECURITY: Number,
+    DISTRIBUTION_POLICY: String,
+    FCODE: Number,
+    FTYPE: Number,
+    GLOBALID: String,
+    GNIS_ID: String,
+    HIKE: Number,
+    LENGTH: Number,
+    LOADDATE: Date,
+    MOTORCYCLE: Number,
+    NAME: String,
+    NONMOTORIZEDONLY: Number,
+    OBJECTID: String,
+    PACKORSADDLE: Number,
+    PERMANENT_IDENTIFIER: String,
+    WIDTH: Number
+  }
 }, {collection: 'ESRI_trials'})
 
 module.exports = mongoose.model('esri-trail', EsriTrail)
