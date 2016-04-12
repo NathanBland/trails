1680c5b323e5403d2f8dd803c62f837782b24b35
Nathan Bland <nathan.bland@gmail.com>
2016-04-12 15:25:54 -0600

    more work to try and fix NaN

--- client/components/list.jsx
+++ client/components/list.jsx
@@ -30,9 +30,9 @@ const list = ({
                 <span>
                   distance: 
                 </span>
-                { isNaN(Number(trail.LENGTH) || Number(trail.Lgth_Miles) || (0.621371 * trail.length_km))
-                ? Number(trail.LENGTH).toFixed(2) || Number(trail.Lgth_Miles).toFixed(2) || (0.621371 * trail.length_km).toFixed(2) } Miles
-                : 'Unknown'
+                { isNaN(Number(trail.Lgth_Miles) || (0.621371 * trail.length_km))
+                ? 'Unknown'
+                : Number( Number(trail.Lgth_Miles).toFixed(2) || (0.621371 * trail.length_km).toFixed(2) } Miles
               </li>
               {/*
                 <li>
