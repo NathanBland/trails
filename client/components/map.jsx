711a97a029a61b6ace4aad7af68b19c7e4690da0
Nathan Bland <nathan.bland@gmail.com>
2016-04-12 15:31:12 -0600

    more work to try and fix NaN

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -50,11 +50,10 @@ function eachFeature(feature, layer){
     }
     </h4>
     <p className='trail__desc'>
-    ${ isNaN(Number(feature.properties.LENGTH).toFixed(2) || 
-        Number(feature.properties.Lgth_Miles).toFixed(2) 
+    ${ isNaN(Number(feature.properties.Lgth_Miles).toFixed(2) 
         || (0.621371 * feature.properties.length_km).toFixed(2))
       ? 'Unkown'
-    : Number(feature.properties.LENGTH).toFixed(2) || Number(trail.Lgth_Miles).toFixed(2) || (0.621371 * trail.length_km).toFixed(2) } Miles
+    : Number(trail.Lgth_Miles).toFixed(2) || (0.621371 * trail.length_km).toFixed(2) } Miles
     <p/>
   `)
 }
