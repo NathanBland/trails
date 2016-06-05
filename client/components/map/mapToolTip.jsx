c15136f5510add45a37ecbfabc0945a5ac93700b
Joel <jgimbel@live.com>
2016-06-05 16:02:45 -0500

    refactor distance calculation

--- client/components/map/mapToolTip.jsx
+++ client/components/map/mapToolTip.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Popup } from 'react-leaflet'
-const Tooltip = ({distance, trailName, isActive, ...props}) => (
-  <Popup {...props}>
-    <span className='trail__tooltip'>
-      <h5>
-        { trailName || 'No name given' }
-      </h5>
-      <p>Estimated Distance: {isActive 
-      ? guessLength(props.popupContainer, distance.addLength) + 'Miles'
-      : '' 
-      }
-      </p>
-    </span>
-  </Popup>
-)
 
-function guessLength(layer, addLength) { //get the id out of the layer?
-  let tempLatLng = null
-  const { _latlngs, feature } = layer._layers[layer._leaflet_id-1]
-  const distance = _latlngs.reduce((totalDistance, latlng)=> {
-    if(tempLatLng == null){
+export default class Tooltip extends Component {
+
+  componentWillMount(){
+    this.guessLength()
+  }
+
+  guessLength() { //get the id out of the layer?
+    if(this.props.id in this.props.distances) return // we already mounted this guy once
+
+    const { popupContainer: layer, distance: { addLength } } = this.props
+    const { _latlngs, feature } = layer._layers[layer._leaflet_id-1]
+    let tempLatLng = null
+
+    const distance = _latlngs.reduce((totalDistance, latlng)=> {
+      if(tempLatLng == null){
         tempLatLng = latlng
         return totalDistance
       }
@@ -27,9 +23,24 @@ function guessLength(layer, addLength) { //get the id out of the layer?
       tempLatLng = latlng
       return totalDistance
     }, 0)
+
     const convertedDistance = (0.000621371 * distance).toFixed(2)
     addLength(feature._id, convertedDistance)
     return convertedDistance
-}
+  }
 
-export default Tooltip
+  render(){
+    const { trailName, distances, id, ...props } = this.props
+    return (
+      <Popup {...props}>
+      <span className='trail__tooltip'>
+        <h5>
+          { trailName || 'No name given' }
+        </h5>
+        <p>Estimated Distance: {distances[id]} Miles
+        </p>
+      </span>
+    </Popup>
+    )
+  }
+}
