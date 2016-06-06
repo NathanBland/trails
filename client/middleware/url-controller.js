29271a2109db1b5053e11d340534ead92efa7a22
Joel Gimbel <jgimbel@live.com>
2016-06-06 12:17:24 -0500

    set active id

--- client/middleware/url-controller.js
+++ client/middleware/url-controller.js
@@ -1,16 +1,14 @@
 export function updateURL (store){
   return next => action => {
     // update the current url
-    if(action.type !== 'SET_MAP'){
-      return next(action)
-    } else {
+    if(action.type === 'SET_MAP' || action.type === 'SET_ACTIVE'){
       const results = next(action)
       const state = store.getState()
-      window.location.hash = `${state.map.center},${state.map.zoom}`
-      if (state.map.active !== '') {
-        //window.location.hash += `,${state.map.active}`
-      }
+      window.location.hash = `${state.map.center},${state.map.zoom}${state.map.active?','+state.map.active:''}`
       return results
+    } else {
+      return next(action)
+
     }
   }
 }
